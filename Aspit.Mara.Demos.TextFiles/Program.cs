a30e45805a9778dd1b7cae955181272236d867cc
Mads Mikkel Rasmussen <mara@aspit.dk>
2018-11-06 09:54:02 +0100

    Init & Done.

--- Aspit.Mara.Demos.TextFiles/Program.cs
+++ Aspit.Mara.Demos.TextFiles/Program.cs
@@ -1,15 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* Demonstrates how to use StreamReader and StreamWriter to read and write to and from a textfile.
+ * StreamReader: https://docs.microsoft.com/da-dk/dotnet/api/system.io.streamreader?view=netframework-4.7.2
+ * StreamWriter: https://docs.microsoft.com/da-dk/dotnet/api/system.io.streamwriter?view=netframework-4.7.2 */
 
-namespace Aspit.Mara.Demos.TextFiles
+using System;
+using System.IO;
+
+namespace Aspit.Mara.Demos.TextFileIO
 {
-    class Program
+    class TextFileIoProgram
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string textToWrite = "This is the content of the file";
+            string directory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            string filename = "testFile";
+            string fileExtension = ".txt";
+            string path = Path.Combine(directory, filename + fileExtension);
+
+            try
+            {
+                using(StreamWriter writer = new StreamWriter(path))
+                {
+                    writer.Write(textToWrite);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error writing to file. {e.Message}");
+            }
+
+            string textToRead;
+
+            try
+            {
+                using(StreamReader reader = new StreamReader(path))
+                {
+                    textToRead = reader.ReadToEnd();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error reading from file. {e.Message}");
+            }
+            Console.ReadLine();
         }
     }
 }
